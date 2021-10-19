1c18e2b8524aabc1e92c1cea3a0fbc36b1d5620f
Patrick Jimison <pat.jimison@gmail.com>
2021-10-18 21:42:04 -0700

    Added PongPaddles which are draggable up and down the y axis

--- main.py
+++ main.py
@@ -28,21 +28,49 @@ class PongBall(Widget):
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            bounced = Vector(-1 * vx, vy)
+            new_vel = bounced
+            ball.velocity = new_vel.x, new_vel.y
+    
+
 class PongGame(Widget):
     ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
 
-    def serve_ball(self):
+    def serve_ball(self, vel=(4,0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4,0).rotate(randint(0,360))
+        self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
+
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
         # bounce off top and bottom
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
-        # bounce off left and right
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
+        
+        # ball hits either side - score a point for the opposite player
+        if self.ball.x < 0:
+            self.player2.score += 1
+            self.serve_ball(vel=(4,0))
+        if self.ball.right > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4,0))
+    
+    def on_touch_move(self, touch):
+        if touch.x < self.width/3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width/3:
+            self.player2.center_y = touch.y
 
 
 if __name__ == '__main__':
